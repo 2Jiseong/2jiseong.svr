99678bbfd355e74548f6bc152d7843bb96e70a60
2Jiseong <2Jiseong@github.com>
2018-01-12 10:40:35 +0900

    수정3

--- callee.js
+++ callee.js
@@ -101,7 +101,7 @@ function onStart() {
     
     local_peer.ondatachannel = cbDtatChannel;
 
-    var url = 'ws://jiseong-svr-express-2.herokuapp.com/room/' + roodId.value;
+    var url = 'wss://jiseong-svr-express-2.herokuapp.com/room/' + roodId.value;
     // var url = 'wss://zoops-webrtc-01.herokuapp.com/room/' + roodId.value;
     g_mc_ws_component.connect(url, onWsMessage);
 
--- caller.js
+++ caller.js
@@ -116,7 +116,7 @@ function onStart() {
         document.querySelector("div#receive").innerHTML += '<br/>' + event.data;
     };
 
-    var url = 'ws://jiseong-svr-express-2.herokuapp.com/room/' + roodId.value;
+    var url = 'wss://jiseong-svr-express-2.herokuapp.com/room/' + roodId.value;
     // var url = 'wss://zoops-webrtc-01.herokuapp.com/room/' + roodId.value;
     g_mc_ws_component.connect(url, onWsMessage);
     
